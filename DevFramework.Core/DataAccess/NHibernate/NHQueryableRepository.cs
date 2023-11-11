38342295ce95899ad6c9dd47adacb50ead60d381
TolgaKmbl <tolgakumbul@gmail.com>
2023-11-11 14:42:11 +0300

    NHibernate impl

---
+++ DevFramework.Core/DataAccess/NHibernate/NHQueryableRepository.cs
@@ -0,0 +1,32 @@
+﻿using DevFramework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.DataAccess.NHibernate
+{
+    public class NHQueryableRepository<T> : IQueryableRepository<T>
+        where T : class, IEntity, new()
+    {
+
+        private readonly NHibernateHelper _nHibernateHelper;
+        private IQueryable<T> entities;
+
+        public NHQueryableRepository(NHibernateHelper nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+
+        public IQueryable<T> Table => this.Entities;
+
+        public virtual IQueryable<T> Entities
+        {
+            get 
+            {
+                return entities ??= _nHibernateHelper.OpenSession().Query<T>();
+            }
+        }
+    }
+}
