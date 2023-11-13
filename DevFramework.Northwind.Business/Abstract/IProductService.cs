587aa79b84659d5c0389ec3adef6185c4c4cf4ad
TolgaKmbl <tolgakumbul@gmail.com>
2023-11-13 22:23:28 +0300

    business layer and validation aspect

---
+++ DevFramework.Northwind.Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        Product GetById(int productId);
+        Product Insert(Product product);
+        Product Update(Product product);
+    }
+}
---
+++ DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,46 @@
+﻿using DevFramework.Core.Aspects.Postsharp;
+using DevFramework.Core.CrossCuttingConcerns.Validation.FluentValidation;
+using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.Business.ValidationRules.FluentValidation;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.Concrete.Managers
+{
+    public class ProductManager : IProductService
+    {
+        private readonly IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
+        }
+
+        [FluentValidationAspect(typeof(ProductValidator))]
+        public Product Insert(Product product)
+        {
+            return _productDal.Add(product);
+        }
+
+        [FluentValidationAspect(typeof(ProductValidator))]
+        public Product Update(Product product)
+        {
+            return _productDal.Update(product);
+        }
+    }
+}
