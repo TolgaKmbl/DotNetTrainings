2bf4bf97cb3bcdc0bf2d2d74595457f450252974
TolgaKmbl <tolgakumbul@gmail.com>
2023-11-13 17:15:33 +0300

    added joined entity dto and dal impl

--- DevFramework.Northwind.DataAccess/Concrete/NHibernate/NHProductDal.cs
+++ DevFramework.Northwind.DataAccess/Concrete/NHibernate/NHProductDal.cs
@@ -1,5 +1,7 @@
 ﻿using DevFramework.Core.DataAccess.NHibernate;
 using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.DataAccess.Concrete.EntityFramework;
+using DevFramework.Northwind.Entities.ComplexTypes;
 using DevFramework.Northwind.Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -11,8 +13,27 @@ namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate
 {
     public class NHProductDal : NHEntityRepositoryBase<Product>, IProductDal
     {
+        private NHibernateHelper _nHibernateHelper;
         public NHProductDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
         {
+            _nHibernateHelper = nHibernateHelper;
+        }
+
+        public List<ProductDetail> GetProductDetails()
+        {
+            using (var session = _nHibernateHelper.OpenSession())
+            {
+                var result = from p in session.Query<Product>()
+                             join c in session.Query<Category>()
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetail
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName
+                             };
+                return result.ToList();
+            }
         }
     }
 }
