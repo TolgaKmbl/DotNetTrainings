e10ddc22fcb05c2660b045d9b79101bbe3eae0f0
TolgaKmbl <tolgakumbul@gmail.com>
2023-11-14 13:40:40 +0300

    Fluent Validation test

--- DevFramework.Core/CrossCuttingConcerns/Validation/FluentValidation/ValidatorTool.cs
+++ DevFramework.Core/CrossCuttingConcerns/Validation/FluentValidation/ValidatorTool.cs
@@ -11,7 +11,8 @@ namespace DevFramework.Core.CrossCuttingConcerns.Validation.FluentValidation
     {
         public static void FluentValidate(IValidator validator, object entity)
         {
-            var result = validator.Validate((IValidationContext)entity);
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
             if (result.Errors.Count > 0)
             {
                 throw new ValidationException(result.Errors);
---
+++ DevFramework.Northwind.Business.Tests/ProductManagerTests.cs
@@ -0,0 +1,24 @@
+﻿using DevFramework.Northwind.Business.Concrete.Managers;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace DevFramework.Northwind.Business.Tests
+{
+    [TestClass]
+    public class ProductManagerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ValidationException))]
+        public void Product_Validation_Test()
+        {
+            Mock<IProductDal> mockProductDal = new Mock<IProductDal>();
+            ProductManager productManager = new ProductManager(mockProductDal.Object);
+
+            productManager.Insert(new Product());
+        }
+    }
+}
