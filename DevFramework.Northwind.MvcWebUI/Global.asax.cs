0fc5649c595ecd27bc9c5abcb69926ed8af7c01d
TolgaKmbl <tolgakumbul@gmail.com>
2023-11-30 12:52:11 +0300

    calling WCF service from MVCWebUI

--- DevFramework.Northwind.MvcWebUI/Global.asax.cs
+++ DevFramework.Northwind.MvcWebUI/Global.asax.cs
@@ -20,7 +20,7 @@ namespace DevFramework.Northwind.MvcWebUI
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
-            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory(new BusinessModule(), new AutoMapperModule()));
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory(new ServiceModule(),/*new BusinessModule(),*/ new AutoMapperModule()));
         }
 
         public override void Init()
